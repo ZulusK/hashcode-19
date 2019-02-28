1ae551313e4629b978632f78aedcb7114712783e
Danil Kazimirov <danil.kazim.99@gmail.com>
2019-02-28 20:44:00 +0200

    fix lint

--- parser.js
+++ parser.js
@@ -1,38 +1,41 @@
-const fs = require('fs');
-
 const fileName = process.argv[process.argv.length - 1];
-let photos = [];
-let numLines = 0;
-let id = 0;
-var readline = require('linebyline'),
-      rl = readline(`./${fileName}`);
-rl.on('line', function(line, lineCount, byteCount) {
-    if(lineCount !== 1){
-        let photoObj = {
-            id: 0,
-            o: '',
-            tags: [], 
-        };
-        let arr = line.split(' ');
+const readline = require('linebyline');
 
-        for(let i = 0; i < arr.length; i++){
-            if(i === 0) {
-                photoObj.id = id;
-                id++;
-                photoObj.o = arr[i];
+function readDataFile(filename) {
+    const rl = readline(`./${fileName}`);
+    return new Promise((resolve, reject) => {
+        let photos = [];
+        let numLines = 0;
+        let id = 0;
+        rl.on('line', (line, lineCount) => {
+            if (lineCount !== 1) {
+                let photoObj = {
+                    id: 0,
+                    o: '',
+                    tags: [],
+                };
+                let arr = line.split(' ');
+
+                for (let i = 0; i < arr.length; i++) {
+                    if (i === 0) {
+                        photoObj.id = id;
+                        id++;
+                        photoObj.o = arr[i];
+                    } else if (i > 1) photoObj.tags.push(arr[i]);
+                }
+                photos.push(photoObj);
+            } else {
+                numLines = parseInt(line);
             }
-            else if(i > 1) photoObj.tags.push(arr[i]);
-            else continue;
-        }
-        photos.push(photoObj);
-    }else{
-        numLines = parseInt(line);
-    }
+            if (lineCount > numLines) {
+                return resolve(photos);
+            }
+        })
+            .on('error', (e) => {
+                console.log(e.message);
+                reject(e);
+            });
+    });
+}
 
-    if(lineCount > numLines){
-        console.log(photos);
-    }
-})
-.on('error', function(e) {
-    console.log(e.message);
-});
+readDataFile(fileName).then(console.log);
