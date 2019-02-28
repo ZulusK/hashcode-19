bdb230af58d665cd8059da87466beeda6d363bbb
Kostia Lelikov <lelikov.kostia1@gmail.com>
2019-02-28 20:53:55 +0200

    Compare

--- parser.js
+++ parser.js
@@ -5,34 +5,33 @@ let photos = [];
 let numLines = 0;
 let id = 0;
 var readline = require('linebyline'),
-      rl = readline(`./${fileName}`);
-rl.on('line', function(line, lineCount, byteCount) {
-    if(lineCount !== 1){
+    rl = readline(`./${fileName}`);
+rl.on('line', function (line, lineCount, byteCount) {
+    if (lineCount !== 1) {
         let photoObj = {
             id: 0,
             o: '',
-            tags: [], 
+            tags: [],
         };
         let arr = line.split(' ');
 
-        for(let i = 0; i < arr.length; i++){
-            if(i === 0) {
+        for (let i = 0; i < arr.length; i++) {
+            if (i === 0) {
                 photoObj.id = id;
                 id++;
                 photoObj.o = arr[i];
-            }
-            else if(i > 1) photoObj.tags.push(arr[i]);
+            } else if (i > 1) photoObj.tags.push(arr[i]);
             else continue;
         }
         photos.push(photoObj);
-    }else{
+    } else {
         numLines = parseInt(line);
     }
 
-    if(lineCount > numLines){
+    if (lineCount > numLines) {
         console.log(photos);
     }
 })
-.on('error', function(e) {
-    console.log(e.message);
-});
+    .on('error', function (e) {
+        console.log(e.message);
+    });
